39e0d505807c4307f712ad267c1b2935aadc8e4c
William Griffith <William Griffith>
2017-10-12 20:46:34 -0400

    Griffith Week 3 Assignment i

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The functions below take a matrix as an input and save it, then it takes the inverse of
+##that matrix and prints it out from the cache, if one doesnt exist in the cache then
+## it calcuates the inverse of that matrix. 
 
-## Write a short comment describing this function
+## This function takes a matrix and caches it
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  
+inverseMatrix <- NULL
+
+setMatrix <- function(y) {
+  x <<- y
+  inverseMatrix <<- NULL
 }
+getMatrix <- function() x
+setInvMatrix <- function(input) inverseMatrix <<- input
+getInvMatrix <- function() inverseMatrix
 
+list <- list(setMat = setMatrix, getMat = getMatrix, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 
-## Write a short comment describing this function
+}
+## This calls the cached matrix and checks to see if it exists, if so it returns the cached inverse and prints, if not it 
+## calculates the inverse and it prints it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+  inverseMatrix <- x$getInvMatrix()
+  if (!is.null(inverseMatrix)) {
+    print("fetching cached matrix")
+    return(inverseMatrix)
+   
+  }
+  matrix <- x$getMat()
+
+  inverseMatrix <- solve(matrix, ...)
+  x$setInvMatrix(inverseMatrix)
+  inverseMatrix
+  
+
 }
+
+matrix <- matrix(1:4, 2)
+cachedMat <- makeCacheMatrix(matrix)
+result <- cacheSolve(cachedMat)
+print(result)
